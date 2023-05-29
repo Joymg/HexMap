4d01373d017bf108ff988005a71aed55eabc4394
Joymg <joy.marquezg@gmail.com>
2023-05-29 14:15:18 +0200

    Reconnecting Cliffs and Terraces

--- Assets/_Project/Scripts/HexMesh.cs
+++ Assets/_Project/Scripts/HexMesh.cs
@@ -240,7 +240,7 @@ namespace joymg
             {
                 b = -b;
             }
-            Vector3 boundary = Vector3.Lerp(begin, right, b);
+            Vector3 boundary = Vector3.Lerp(Perturb(begin), Perturb(right), b);
             Color boundaryColor = Color.Lerp(beginCell.Color, rightCell.Color, b);
 
             TriangulateBoundaryTriangle(
@@ -253,7 +253,7 @@ namespace joymg
             }
             else
             {
-                AddTriangle(left, right, boundary);
+                AddTriangleUnperturbed(Perturb(left), Perturb(right), boundary);
                 AddTriangleColor(leftCell.Color, rightCell.Color, boundaryColor);
             }
         }
@@ -269,7 +269,7 @@ namespace joymg
             {
                 b = -b;
             }
-            Vector3 boundary = Vector3.Lerp(begin, left, b);
+            Vector3 boundary = Vector3.Lerp(Perturb(begin), Perturb(left), b);
             Color boundaryColor = Color.Lerp(beginCell.Color, leftCell.Color, b);
 
             TriangulateBoundaryTriangle(
@@ -282,7 +282,7 @@ namespace joymg
             }
             else
             {
-                AddTriangle(left, right, boundary);
+                AddTriangleUnperturbed(Perturb(left), Perturb(right), boundary);
                 AddTriangleColor(leftCell.Color, rightCell.Color, boundaryColor);
             }
         }
@@ -291,25 +291,24 @@ namespace joymg
             Vector3 begin, HexCell beginCell,
             Vector3 left, HexCell leftCell,
             Vector3 boundary, Color boundaryColor
-        )
-        {
-            Vector3 v2 = HexMetrics.TerraceLerp(begin, left, 1);
+        ) {
+            Vector3 v2 = Perturb(HexMetrics.TerraceLerp(begin, left, 1));
             Color c2 = HexMetrics.TerraceLerp(beginCell.Color, leftCell.Color, 1);
 
-            AddTriangle(begin, v2, boundary);
+            AddTriangleUnperturbed(Perturb(begin), v2, boundary);
             AddTriangleColor(beginCell.Color, c2, boundaryColor);
 
             for (int i = 2; i < HexMetrics.terraceSteps; i++)
             {
                 Vector3 v1 = v2;
                 Color c1 = c2;
-                v2 = HexMetrics.TerraceLerp(begin, left, i);
+                v2 = Perturb(HexMetrics.TerraceLerp(begin, left, i));
                 c2 = HexMetrics.TerraceLerp(beginCell.Color, leftCell.Color, i);
-                AddTriangle(v1, v2, boundary);
+                AddTriangleUnperturbed(v1, v2, boundary);
                 AddTriangleColor(c1, c2, boundaryColor);
             }
 
-            AddTriangle(v2, left, boundary);
+            AddTriangleUnperturbed(v2, Perturb(left), boundary);
             AddTriangleColor(c2, leftCell.Color, boundaryColor);
         }
 
@@ -343,6 +342,17 @@ namespace joymg
             vertices.Add(Perturb(v1));
             vertices.Add(Perturb(v2));
             vertices.Add(Perturb(v3));
+            triangles.Add(vertexIndex);
+            triangles.Add(vertexIndex + 1);
+            triangles.Add(vertexIndex + 2);
+        }
+
+        private void AddTriangleUnperturbed(Vector3 v1, Vector3 v2, Vector3 v3)
+        {
+            int vertexIndex = vertices.Count;
+            vertices.Add(v1);
+            vertices.Add(v2);
+            vertices.Add(v3);
             triangles.Add(vertexIndex);
             triangles.Add(vertexIndex + 1);
             triangles.Add(vertexIndex + 2);
